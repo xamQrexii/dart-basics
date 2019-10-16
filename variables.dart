ffe20627fffd32f3265f6cbce69d7582d1571eff
xamqrexii <usama1630@gmail.com>
2019-10-17 01:04:08 +0500

    Variables in dart

---
+++ variables.dart
@@ -0,0 +1,25 @@
+void main() {
+  
+//   print, print single value at a time
+//   terminator important
+  print('Hello World!');
+//   concatenation
+  print('Hello World!' + ' this is another string');
+
+//   variables
+
+  int age = 29;
+  String name = "Muhammad Osama";
+  bool isActive = true;
+  double price = 30.55;
+
+//   it will define type automatically
+  var friendName = 'Anas Ahmed Khan Niazi';
+// cant overrite with diff type: friendName = 45;
+
+//   dynamic type
+  dynamic student = "Syed Ammar Jamil";
+//   overwrite any data type value
+  student = 34;
+
+}
