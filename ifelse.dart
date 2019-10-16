37876f0e2321df88e0f8b10f4782a863d0515d2c
xamqrexii <usama1630@gmail.com>
2019-10-17 01:06:09 +0500

    if else in dart

---
+++ ifelse.dart
@@ -0,0 +1,18 @@
+void main() {
+
+  bool isActive = false;
+
+  //   if else
+  if (!isActive) {
+    print('This is true');
+  } else {
+    print('This is false');
+  }
+
+  if (age > 40) {
+    print('Age is greater than 29');
+  } else {
+    print('Age is not greater than 29');
+  }
+
+}
